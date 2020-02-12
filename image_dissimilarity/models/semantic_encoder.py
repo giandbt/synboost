7628c1535ea2fdbacc5bc4fbc826043003764645
Giancarlo <giancarlo.dibiase@advertima.com>
2020-02-12 14:35:41 +0100

    dataloaders and encoders working

--- image_dissimilarity/models/semantic_encoder.py
+++ image_dissimilarity/models/semantic_encoder.py
@@ -1,10 +1,8 @@
-from PIL import Image
 import torch.nn as nn
-import torchvision.transforms as transforms
-import torch
 
 class SemanticEncoder(nn.Module):
 	''' Semantic Encoder as described in Detecting the Unexpected via Image Resynthesis '''
+	
 	def __init__(self, in_channels=3):
 		super(SemanticEncoder, self).__init__()
 		self.relu = nn.ReLU(True)
@@ -18,10 +16,15 @@ class SemanticEncoder(nn.Module):
 	def _initialize_weights(self):
 		for m in self.modules():
 			if isinstance(m, nn.Conv2d):
-				m.weight.data.normal_(0, 0.01)
+				nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+				if m.bias is not None:
+					nn.init.constant_(m.bias, 0)
 			elif isinstance(m, nn.BatchNorm2d):
-				m.weight.data.fill_(1)
-				m.bias.data.zero_()
+				nn.init.constant_(m.weight, 1)
+				nn.init.constant_(m.bias, 0)
+			elif isinstance(m, nn.Linear):
+				nn.init.normal_(m.weight, 0, 0.01)
+				nn.init.constant_(m.bias, 0)
 	
 	def forward(self, x):
 		x = self.conv1(x)
@@ -34,13 +37,18 @@ class SemanticEncoder(nn.Module):
 		x4 = self.relu(x)
 		return [x1, x2, x3, x4]
 
-img = Image.open('../../sample_images/zm0002_100000.png')
-test = SemanticEncoder()
-img_transform = transforms.Compose([transforms.ToTensor()])
-img_tensor = img_transform(img)
-outputs = test(img_tensor.unsqueeze(0))
-print(img_tensor[0].data.shape)
-print(outputs[0].data.shape)
-print(outputs[1].data.shape)
-print(outputs[2].data.shape)
-print(outputs[3].data.shape)
+
+if __name__ == "__main__":
+	from PIL import Image
+	import torchvision.transforms as transforms
+	
+	img = Image.open('../../sample_images/zm0002_100000.png')
+	test = SemanticEncoder()
+	img_transform = transforms.Compose([transforms.ToTensor()])
+	img_tensor = img_transform(img)
+	outputs = test(img_tensor.unsqueeze(0))
+	print(img_tensor[0].data.shape)
+	print(outputs[0].data.shape)
+	print(outputs[1].data.shape)
+	print(outputs[2].data.shape)
+	print(outputs[3].data.shape)
