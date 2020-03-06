e40b9f335f52bb526d719c65d13d2bfe64ca4e41
Giancarlo <giancarlo.dibiase@advertima.com>
2020-03-06 13:37:19 +0100

    corrected visualization params

--- image_dissimilarity/test.py
+++ image_dissimilarity/test.py
@@ -166,15 +166,16 @@ print("roc_auc_score : " + str(results['auroc']))
 print("mAP: " + str(results['AP']))
 print("FPR@95%TPR : " + str(results['FPR@95%TPR']))
 
-plt.figure()
-lw = 2
-plt.plot(fpr, tpr, color='darkorange',
-         lw=lw, label='ROC curve (area = %0.2f)' % results['auroc'])
-plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.0])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic Curve')
-plt.legend(loc="lower right")
-plt.savefig(os.path.join(store_fdr,'roc_curve.png'))
+if config['visualize']:
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % results['auroc'])
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic Curve')
+    plt.legend(loc="lower right")
+    plt.savefig(os.path.join(store_fdr,'roc_curve.png'))
