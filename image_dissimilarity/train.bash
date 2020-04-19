47ba2e759cb4f89f24ccb92793a338063709850c
giandbt <giandbt@gmail.com>
2020-04-19 18:58:28 +0200

    minor organization mod

--- image_dissimilarity/train.bash
+++ image_dissimilarity/train.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=5 python train.py --config configs/train/default_configuration.yaml --gpu_ids 0
+CUDA_VISIBLE_DEVICES=15 python train.py --config configs/train/default_configuration.yaml --gpu_ids 0
