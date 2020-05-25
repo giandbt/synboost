5d030cfb41084bebed3d67e6ea0fd4f0d294627d
Giancarlo <giancarlo.dibiase@advertima.com>
2020-05-25 13:13:48 +0200

    updated test config files for newer architecture

--- image_dissimilarity/test.bash
+++ image_dissimilarity/test.bash
@@ -1,2 +1,4 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=15 python test.py --config configs/test/road_anomaly_configuration.yaml --gpu_ids 0
+CUDA_VISIBLE_DEVICES=13 python test.py --config configs/test/road_anomaly_configuration.yaml --gpu_ids 0
+CUDA_VISIBLE_DEVICES=13 python test.py --config configs/test/fs_static_configuration.yaml --gpu_ids 0
+CUDA_VISIBLE_DEVICES=13 python test.py --config configs/test/fs_lost_found_configuration.yaml --gpu_ids 0
