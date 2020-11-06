6adf5e80bcc9630a33bda7efa92c1075ba3e157a
Giancarlo <giancarlo.dibiase@advertima.com>
2020-11-06 09:10:20 +0100

    cleaning config files for replication

--- image_dissimilarity/test.bash
+++ image_dissimilarity/test.bash
@@ -1,12 +1,4 @@
 #!/bin/bash
-#CUDA_VISIBLE_DEVICES=14 python test_multiple.py --config configs/test/road_anomaly_configuration.yaml --gpu_ids 0 --num_exp 3
-#CUDA_VISIBLE_DEVICES=14 python test_multiple.py --config configs/test/fs_static_configuration.yaml --gpu_ids 0 --num_exp 3
-#CUDA_VISIBLE_DEVICES=14 python test_multiple.py --config configs/test/fs_lost_found_configuration.yaml --gpu_ids 0 --num_exp 3
-
-CUDA_VISIBLE_DEVICES=15 python test.py --config configs/test/road_anomaly_configuration.yaml --gpu_ids 0
-CUDA_VISIBLE_DEVICES=15 python test.py --config configs/test/fs_static_configuration.yaml --gpu_ids 0
-CUDA_VISIBLE_DEVICES=15 python test.py --config configs/test/fs_lost_found_configuration.yaml --gpu_ids 0
-
-
-#CUDA_VISIBLE_DEVICES=15 python test_ensemble.py --config configs/test/fs_static_configuration.yaml --gpu_ids 0
-#CUDA_VISIBLE_DEVICES=15 python test_ensemble.py --config configs/test/fs_lost_found_configuration.yaml --gpu_ids 0
+CUDA_VISIBLE_DEVICES=0 python test.py --config configs/test/road_anomaly_configuration.yaml --gpu_ids 0
+CUDA_VISIBLE_DEVICES=0 python test.py --config configs/test/fs_static_configuration.yaml --gpu_ids 0
+CUDA_VISIBLE_DEVICES=0 python test.py --config configs/test/fs_lost_found_configuration.yaml --gpu_ids 0
--- image_dissimilarity/test.py
+++ image_dissimilarity/test.py
@@ -15,7 +15,10 @@ from models.dissimilarity_model import DissimNet, DissimNetPrior
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, help='Path to the config file.')
-parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+parser.add_argument('--gpu_ids',
+                    type=str,
+                    default='0',
+                    help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
 opts = parser.parse_args()
 cudnn.benchmark = True
 
@@ -28,19 +31,22 @@ exp_name = config['experiment_name']
 save_fdr = config['save_folder']
 epoch = config['which_epoch']
 store_fdr = config['store_results']
-store_fdr_exp = os.path.join(config['store_results'],exp_name)
+store_fdr_exp = os.path.join(config['store_results'], exp_name)
 ensemble = config['ensemble']
 
 if not os.path.isdir(store_fdr):
-    os.mkdir(store_fdr)
+    os.makedirs(store_fdr, exist_ok=True)
 
 if not os.path.isdir(store_fdr_exp):
-    os.mkdir(store_fdr_exp)
+    os.makedirs(
+        store_fdr_exp,
+        exist_ok=True,
+    )
 
 if not os.path.isdir(os.path.join(store_fdr_exp, 'pred')):
-    os.mkdir(os.path.join(store_fdr_exp, 'label'))
-    os.mkdir(os.path.join(store_fdr_exp, 'pred'))
-    os.mkdir(os.path.join(store_fdr_exp, 'soft'))
+    os.makedirs(os.path.join(store_fdr_exp, 'label'), exist_ok=True)
+    os.makedirs(os.path.join(store_fdr_exp, 'pred'), exist_ok=True)
+    os.makedirs(os.path.join(store_fdr_exp, 'soft'), exist_ok=True)
 
 # Activate GPUs
 config['gpu_ids'] = opts.gpu_ids
@@ -52,7 +58,8 @@ prior = config['model']['prior']
 cfg_test_loader = config['test_dataloader']
 # adds logic to dataloaders (avoid repetition in config file)
 cfg_test_loader['dataset_args']['prior'] = prior
-test_loader = trainer_util.get_dataloader(cfg_test_loader['dataset_args'], cfg_test_loader['dataloader_args'])
+test_loader = trainer_util.get_dataloader(cfg_test_loader['dataset_args'],
+                                          cfg_test_loader['dataloader_args'])
 
 # get model
 if config['model']['prior']:
@@ -63,7 +70,8 @@ else:
     raise NotImplementedError()
 
 diss_model.eval()
-model_path = os.path.join(save_fdr, exp_name, '%s_net_%s.pth' %(epoch, exp_name))
+model_path = os.path.join(save_fdr, exp_name,
+                          '%s_net_%s.pth' % (epoch, exp_name))
 model_weights = torch.load(model_path)
 diss_model.load_state_dict(model_weights)
 
@@ -71,11 +79,11 @@ softmax = torch.nn.Softmax(dim=1)
 
 # create memory locations for results to save time while running the code
 dataset = cfg_test_loader['dataset_args']
-h = int((dataset['crop_size']/dataset['aspect_ratio']))
+h = int((dataset['crop_size'] / dataset['aspect_ratio']))
 w = int(dataset['crop_size'])
-flat_pred = np.zeros(w*h*len(test_loader), dtype='float32')
-flat_labels = np.zeros(w*h*len(test_loader), dtype='float32')
-num_points=50
+flat_pred = np.zeros(w * h * len(test_loader), dtype='float32')
+flat_labels = np.zeros(w * h * len(test_loader), dtype='float32')
+num_points = 50
 
 with torch.no_grad():
     for i, data_i in enumerate(tqdm(test_loader)):
@@ -83,29 +91,36 @@ with torch.no_grad():
         semantic = data_i['semantic'].cuda()
         synthesis = data_i['synthesis'].cuda()
         label = data_i['label'].cuda()
-    
+
         if prior:
             entropy = data_i['entropy'].cuda()
             mae = data_i['mae'].cuda()
             distance = data_i['distance'].cuda()
-            outputs = softmax(diss_model(original, synthesis, semantic, entropy, mae, distance))
+            outputs = softmax(
+                diss_model(original, synthesis, semantic, entropy, mae,
+                           distance))
         else:
             outputs = softmax(diss_model(original, synthesis, semantic))
-        (softmax_pred, predictions) = torch.max(outputs,dim=1)
+        (softmax_pred, predictions) = torch.max(outputs, dim=1)
         if ensemble:
-            soft_pred = outputs[:,1,:,:]*0.75 + entropy*0.25
+            soft_pred = outputs[:, 1, :, :] * 0.75 + entropy * 0.25
         else:
-            soft_pred = outputs[:,1,:,:]
-        flat_pred[i*w*h:i*w*h+w*h] = torch.flatten(soft_pred).detach().cpu().numpy()
-        flat_labels[i*w*h:i*w*h+w*h] = torch.flatten(label).detach().cpu().numpy()
+            soft_pred = outputs[:, 1, :, :]
+        flat_pred[i * w * h:i * w * h +
+                  w * h] = torch.flatten(soft_pred).detach().cpu().numpy()
+        flat_labels[i * w * h:i * w * h +
+                    w * h] = torch.flatten(label).detach().cpu().numpy()
         # Save results
         predicted_tensor = predictions * 1
         label_tensor = label * 1
-        
+
         file_name = os.path.basename(data_i['original_path'][0])
-        label_img = Image.fromarray(label_tensor.squeeze().cpu().numpy().astype(np.uint8))
-        soft_img = Image.fromarray((soft_pred.squeeze().cpu().numpy()*255).astype(np.uint8))
-        predicted_img = Image.fromarray(predicted_tensor.squeeze().cpu().numpy().astype(np.uint8))
+        label_img = Image.fromarray(
+            label_tensor.squeeze().cpu().numpy().astype(np.uint8))
+        soft_img = Image.fromarray(
+            (soft_pred.squeeze().cpu().numpy() * 255).astype(np.uint8))
+        predicted_img = Image.fromarray(
+            predicted_tensor.squeeze().cpu().numpy().astype(np.uint8))
         predicted_img.save(os.path.join(store_fdr_exp, 'pred', file_name))
         soft_img.save(os.path.join(store_fdr_exp, 'soft', file_name))
         label_img.save(os.path.join(store_fdr_exp, 'label', file_name))
@@ -125,8 +140,11 @@ print("FPR@95%TPR : " + str(results['FPR@95%TPR']))
 if config['visualize']:
     plt.figure()
     lw = 2
-    plt.plot(results['fpr'], results['tpr'], color='darkorange',
-             lw=lw, label='ROC curve (area = %0.2f)' % results['auroc'])
+    plt.plot(results['fpr'],
+             results['tpr'],
+             color='darkorange',
+             lw=lw,
+             label='ROC curve (area = %0.2f)' % results['auroc'])
     plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.0])
@@ -134,4 +152,4 @@ if config['visualize']:
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic Curve')
     plt.legend(loc="lower right")
-    plt.savefig(os.path.join(store_fdr_exp,'roc_curve.png'))
+    plt.savefig(os.path.join(store_fdr_exp, 'roc_curve.png'))
